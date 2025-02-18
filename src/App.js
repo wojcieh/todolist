f2b7ed78fd4255bca600ed3c2d952502dd85af04
Marcin Wojciechowski <marcin.wojciechowski@goleniow.edu.pl>
2025-02-18 13:42:20 +0100

    gf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 function App() {
+  const [zadania, ustawZadanie] = useState([]);
+  const [noweZadanie, dodajNoweZadanie] = useState("");
+  const dodajZadanie = () => {
+    if (noweZadanie.trim()=== "") return;
+    ustawZadanie([...zadania, {id: Date.now(), text: noweZadanie}])
+    dodajNoweZadanie("");
+  }
+  const usunZadanie = (zadanieID) => {
+    ustawZadanie(zadania.filter(zadanie => zadanie.id !== zadanieID));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mt-4 p-4 bg-light rounded shadow'>
+      <h1 className='text-center mb-4'>Lista zadań</h1>
+      <div className='input-group mb-3'>
+      
+      <input onKeyDown={(e) => e.key ==="Enter" && dodajNoweZadanie(e.target.value)} className='form-control' type="text" value={noweZadanie} onChange={(e) => dodajNoweZadanie(e.target.value)}></input><button className='btn btn-primary' onClick={dodajZadanie}>Dodaj</button>
+     </div>
+      <ul className='list-group'>
+        {zadania.map((zadanie) => (
+          <li className="list-group-item d-flex justify-content-between align-items-center"> {zadanie.text}<button className='btn btn-danger' onClick={()=>usunZadanie(zadanie.id)}>usuń</button></li>
+        ))        
+}
+      </ul>
     </div>
   );
 }
